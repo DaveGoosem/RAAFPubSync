9ec3868491be7ef94ca9c5ed9e82aaa2327e3a34
David Goosem <dgoosem@gmail.com>
2013-09-19 20:19:51 +1000

    Initial Commit - basic logic in place

---
+++ RAAFPubSync/Publications.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Publications.aspx.cs" Inherits="RAAFPubSync.Publications" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RAAF Publications Sync</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>        
+            Select publication from the list below:
+        </h2>
+        
+        <p>        
+            <asp:Button ID="getAIPAB" Text="Get AIPAB" OnClick="GetAIPAB_Click" runat="server" />
+        </p>
+        <p>        
+            <asp:Button ID="getFIHA" Text="Get FIHA" OnClick="GetFIHA_Click" runat="server" />
+        </p>      
+        <p>        
+            <asp:Button ID="getGPA" Text="Get GPA" OnClick="GetGPA_Click" runat="server" />
+        </p>        
+        <p>
+            <h3>Download Status Logging</h3>
+            <asp:Label runat="server" ID="statusMessage" name="statusMessage"></asp:Label>
+        </p>              
+    </div>
+    </form>
+</body>
+</html>
---
+++ RAAFPubSync/Publications.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace RAAFPubSync
+{
+    public partial class Publications : System.Web.UI.Page
+    {
+        readonly string _pubParentDirectoryPath = System.Configuration.ConfigurationManager.AppSettings["PubParentDirectoryPath"];
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetRemoteFile("http://www.raafais.gov.au/Pdf/", "AIPAB_", "_web.pdf", "AIPAB");
+            GetRemoteFile("http://www.raafais.gov.au/pdf/", "FIHA_", "_web.pdf", "FIHA");
+            GetRemoteFile("http://www.raafais.gov.au/pdf/", "GPA_", "_web.pdf", "GPA");
+        }
+
+        public void GetAIPAB_Click(object sender, EventArgs e)
+        {
+            GetRemoteFile("http://www.raafais.gov.au/Pdf/", "AIPAB_", "_web.pdf", "AIPAB");
+        }
+
+        public void GetFIHA_Click(object sender, EventArgs e)
+        {
+            GetRemoteFile("http://www.raafais.gov.au/pdf/", "FIHA_", "_web.pdf", "FIHA");
+        }
+
+        public void GetGPA_Click(object sender, EventArgs e)
+        {
+            GetRemoteFile("http://www.raafais.gov.au/pdf/", "GPA_", "_web.pdf", "GPA");
+        }
+
+
+        private void GetRemoteFile(string filePath, string fileNameStart, string fileNameEnd, string fileDestFolderName)
+        {
+            statusMessage.Text += "Download Started <br />";
+
+            foreach (DateTime day in EachDay(DateTime.Now.AddMonths(-1), DateTime.Now))
+            {
+                string fileName = fileNameStart + day.ToString("ddMMMyy") + fileNameEnd;
+
+                if (CheckFileExists((filePath + fileName)))
+                {
+                    //create inner dest directory if doesn't exist
+                    DirectoryInfo dir = new DirectoryInfo(_pubParentDirectoryPath + fileDestFolderName);
+                    if (!dir.Exists)
+                    {
+                        dir.Create();
+                    }                    
+
+                    WebClient client = new WebClient();                    
+                    client.DownloadFile((filePath + fileName), _pubParentDirectoryPath + fileDestFolderName + @"\" + fileName);
+                    statusMessage.Text += "Download Completed from " + filePath + fileName + "<br />";
+                    break;
+                    
+                }
+                else
+                {
+                    statusMessage.Text += "Unable to download - file not found: " + filePath + fileName + "<br />";
+                }
+            }
+        }
+
+
+        public IEnumerable<DateTime> EachDay(DateTime from, DateTime to)
+        {
+            for (var day = from.Date; day.Date <= to.Date; day = day.AddDays(1))
+                yield return day; //this is in standard DateTime format 
+        }
+
+        public bool CheckFileExists(string filePath)
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(filePath);
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
